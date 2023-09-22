797da8e63ed6e2b03433fcc3f3e3c6b1cb914232
TomIO <tomjo00@web.de>
2023-09-22 22:08:32 +0200

    update: libprotobuf and dependents

--- packages/termux-gui-c/build.sh
+++ packages/termux-gui-c/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A C library for the Termux:GUI plugin"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@tareksander"
 TERMUX_PKG_VERSION="0.1.1"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_BUILD_DEPENDS="protobuf-static"
 TERMUX_PKG_SRCURL="https://github.com/tareksander/termux-gui-c-bindings/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=02e5e65bb3bfb0cc27748420af4984faa63cefa02c096b5706c1cf39425890d1
